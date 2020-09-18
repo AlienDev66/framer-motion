ea6b9133e27545ee1192850c71baf20324b1fe39
AlienDev66 <p.capitango65@gmail.com>
2020-09-18 19:15:52 +0100

    :sparkles: Adicionando controle do scroll.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import FirstAndSecond from "./components/FirstAndSecond";
+import { Main, Section, Sticky } from "./style";
 
 import GlobalStyles from "./style/global";
 
 function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
   return (
     <>
       <GlobalStyles />
+      <Main style={{ height: "1610vh" }}>
+        <Section style={{ height: "72.7%" }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: "9.7%" }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: "10.1%" }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
     </>
   );
 }
